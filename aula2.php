04d59e3f2e5cb43ad0b382660f8b30fb573ef011
Érika Morais Xavier <erikausjt@yahoo.com.br>
2021-02-15 20:44:13 -0300

    aula 2 - tipos de dados

---
+++ aula2.php
@@ -0,0 +1,28 @@
+<?php
+
+$nome = "Érika"."<br>";
+
+echo $nome;
+
+/* TIPOS DE DADOS
+
+inteiro
+ponto flutuante
+string
+array
+objeto
+booleanos
+*/
+
+$idade = 35;
+$pi = 3.14;
+$nome = "Érika";
+$acesso = true;
+
+
+echo $idade."<br>";
+echo $pi."<br>";
+echo $nome."<br>";
+echo $acesso."<br>";
+
+?>
